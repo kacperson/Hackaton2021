ed1e3d233c430d65829138612e887a07a297ec39
kacperson <kc@handzlik.pl>
2021-01-16 23:20:39 +0100

    fixe savescreenshot

--- client/screen_to_text/saveScreenshot.py
+++ client/screen_to_text/saveScreenshot.py
@@ -1,9 +1,11 @@
 import PIL
 from PIL import Image
 
-__pictureNumber =0
+__pictureNumber = 0
 imgName="ss"
 
 def saveScreenshot(img):
+    global __pictureNumber
     img.save(imgName + str(__pictureNumber) + ".png", "png")
     __pictureNumber = __pictureNumber+1
+    
--- client/setup.sh
+++ client/setup.sh
@@ -2,3 +2,4 @@ pip install pyautogui
 pip install pytesseract
 pip install speech_recognition
 pip install keyboard
+pip install PIL
