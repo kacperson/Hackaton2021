be8d3bb20c7085da0d33b1a5dd62c4dea912ed0a
kacperson <kc@handzlik.pl>
2021-01-16 22:04:35 +0100

    as

--- client/screen_to_text/ImgToText.py
+++ client/screen_to_text/ImgToText.py
@@ -3,9 +3,11 @@ try:
 except ImportError:
     import Image
 import pytesseract
-
+pytesseract.image_to_string(Image.open("test.png"))
 def ocr_core(filename):
+
     text = pytesseract.image_to_string(Image.open(filename))
+    
     return text
 
-print(ocr_core('C:/Users/kcprh/Pictures/test.png'))
+#print(ocr_core("test.png"))
