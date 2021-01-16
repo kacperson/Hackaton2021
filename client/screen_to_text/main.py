c2e75dc76ce05e5f8b54cbc28f6f30af163aa6c1
Pawel Radzik <pawel.radzik321@gmail.com>
2021-01-16 21:43:18 +0100

    main created

---
+++ client/screen_to_text/main.py
@@ -0,0 +1,24 @@
+import PIL
+import pyautogui
+from PIL import ImageGrab
+
+imgsimilarity=0.90
+
+print("hit enter on left up[ corner")
+input()
+lu=pyautogui.position()
+print("hit enter on right down corner")
+input()
+rd=pyautogui.position()
+print(lu+rd)
+
+pyautogui.sleep(3)
+
+newimg = ImageGrab.grab(lu+rd)
+oldimg = ImageGrab.grab(lu+rd)
+while True:
+    pyautogui.sleep(3)
+    oldimg=newimg
+    newimg = ImageGrab.grab(lu + rd)
+
+
