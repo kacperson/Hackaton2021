ffedb42a8127078a631f5c1d853083b352b811c7
kacperson <kc@handzlik.pl>
2021-01-16 22:38:18 +0100

    Update ImgToText.py

--- client/screen_to_text/ImgToText.py
+++ client/screen_to_text/ImgToText.py
@@ -3,12 +3,9 @@ try:
 except ImportError:
     import Image
 import pytesseract
-<<<<<<< HEAD
 
-pytesseract.image_to_string(Image.open("test.png"))
-=======
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
->>>>>>> 6ef5953c6074fa38dd6bb039423e82e45b5ae00d
+
 def ocr_core(filename):
 
     text = pytesseract.image_to_string(Image.open(filename))
